b44a1ed6588409bcbc07edbdfe436c5231035cf9
zilong-ding <3113660834@qq.com>
2022-06-19 22:05:55 +0800

    fifth commit

---
+++ opencv&ROS2.1/ros-learn.cpp
@@ -0,0 +1,4 @@
+//
+// Created by dzl on 22-6-19.
+//
+#include "ros.cpp"
---
+++ ros-learn/src/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dzl/CLionProjects/-SLAM/ros-learn/src/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/dzl/CLionProjects/-SLAM/ros-learn/src/cmake-build-debug'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/dzl/CLionProjects/-SLAM/ros-learn/src/cmake-build-debug/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/dzl/CLionProjects/-SLAM/ros-learn/src:$ROS_PACKAGE_PATH"
